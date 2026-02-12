5ead78067c17b002601ca0721743424c7a3b3ed5
KotlinJS5 <178154518+KotlinJS5@users.noreply.github.com>
2026-02-11 20:18:35 -0500

    Configure build environment, add app icons, and enable AndroidX

---
+++ app/build/generated/ksp/debug/java/com/multisim/app/MultiSimApplication_GeneratedInjector.java
@@ -0,0 +1,17 @@
+package com.multisim.app;
+
+import dagger.hilt.InstallIn;
+import dagger.hilt.codegen.OriginatingElement;
+import dagger.hilt.components.SingletonComponent;
+import dagger.hilt.internal.GeneratedEntryPoint;
+import javax.annotation.processing.Generated;
+
+@OriginatingElement(
+    topLevelClass = MultiSimApplication.class
+)
+@GeneratedEntryPoint
+@InstallIn(SingletonComponent.class)
+@Generated("dagger.hilt.android.processor.internal.androidentrypoint.InjectorEntryPointGenerator")
+public interface MultiSimApplication_GeneratedInjector {
+  void injectMultiSimApplication(MultiSimApplication multiSimApplication);
+}
---
+++ app/build/kspCaches/debug/backups/java/byRounds/1/dagger/hilt/internal/aggregatedroot/codegen/_com_multisim_app_MultiSimApplication.java
@@ -0,0 +1,21 @@
+package dagger.hilt.internal.aggregatedroot.codegen;
+
+import dagger.hilt.android.HiltAndroidApp;
+import dagger.hilt.internal.aggregatedroot.AggregatedRoot;
+import javax.annotation.processing.Generated;
+
+/**
+ * This class should only be referenced by generated code! This class aggregates information across multiple compilations.
+ */
+@AggregatedRoot(
+    root = "com.multisim.app.MultiSimApplication",
+    rootPackage = "com.multisim.app",
+    originatingRoot = "com.multisim.app.MultiSimApplication",
+    originatingRootPackage = "com.multisim.app",
+    rootAnnotation = HiltAndroidApp.class,
+    rootSimpleNames = "MultiSimApplication",
+    originatingRootSimpleNames = "MultiSimApplication"
+)
+@Generated("dagger.hilt.processor.internal.root.AggregatedRootGenerator")
+public class _com_multisim_app_MultiSimApplication {
+}
